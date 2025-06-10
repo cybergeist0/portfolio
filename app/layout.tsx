52935431e8a84e8484c7829785b9a04204f9fd1e
Satej Gandre <ihs.satej@outlook.com>
2025-06-10 20:06:02 +0000

    started personalizing the nav and footer

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,15 +11,15 @@ import { baseUrl } from './sitemap'
 export const metadata: Metadata = {
   metadataBase: new URL(baseUrl),
   title: {
-    default: 'Next.js Portfolio Starter',
-    template: '%s | Next.js Portfolio Starter',
+    default: 'Satej Gandre',
+    template: '%s | Satej Gandre',
   },
-  description: 'This is my portfolio.',
+  description: 'Satej\'s portfolio.',
   openGraph: {
-    title: 'My Portfolio',
-    description: 'This is my portfolio.',
+    title: 'Satej Gandre',
+    description: 'Satej\'s portfolio.',
     url: baseUrl,
-    siteName: 'My Portfolio',
+    siteName: 'Satej Gandre',
     locale: 'en_US',
     type: 'website',
   },
