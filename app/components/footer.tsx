93c8e88a3ceeb5402b37d12ed630ef881357302c
Satej Gandre <ihs.satej@outlook.com>
2025-06-28 00:27:22 -0400

    configured navbar

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -46,10 +46,10 @@ export default function Footer() {
             className="flex items-center transition-all hover:text-neutral-800 dark:hover:text-neutral-100"
             rel="noopener noreferrer"
             target="_blank"
-            href="https://vercel.com/templates/next.js/portfolio-starter-kit"
+            href="#"
           >
             <ArrowIcon />
-            <p className="ml-2 h-7">view source</p>
+            <p className="ml-2 h-7">youtube</p>
           </a>
         </li>
       </ul>
