9736d6b5f927ca3fffcedadce58ec3758927022c
Satej Gandre <ihs.satej@outlook.com>
2025-06-28 00:32:03 -0400

    changed desc again

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ export default function Page() {
         Satej Gandre
       </h1>
       <p className="mb-4">
-        {`I'm a high school student who enjoys computer science and mathematics, especially when it means solving complex problems or building innovative projects. I've worked with C++, Python, and C#, explored cybersecurity, and dabbled in statistics. Lately, I've been really into competitive programming alongside machine learning.`}
+        {`I'm a high school student who enjoys computer science and mathematics, especially when it comes to solving complex problems or building innovative projects. I've worked with C++ and Python, explored cybersecurity, and dabbled in statistics. Lately, I've been involved in competitive programming alongside machine learning and embedded systems.`}
       </p>
       <div className="my-8">
         <BlogPosts />
